61111cfbd23fb9dba96d5b5f3f2d1ab8932d35e2
mink <frog050406@gmail.com>
2025-01-15 16:26:29 +0900

    feat:first commit

---
+++ spring-project01/src/main/java/study/mini01/MiniProject01Application.java
@@ -0,0 +1,14 @@
+package study.mini01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProject01Application {
+
+	public static void main(String[] args) {
+		int a,b;
+		SpringApplication.run(MiniProject01Application.class, args);
+	}
+
+}
