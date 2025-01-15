02e0762670b71ab739697baca9d2a0c9868f4e6b
mink <frog050406@gmail.com>
2025-01-15 16:31:16 +0900

    test commit&push

--- spring-project01/src/main/java/study/mini01/MiniProject01Application.java
+++ spring-project01/src/main/java/study/mini01/MiniProject01Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MiniProject01Application {
 
 	public static void main(String[] args) {
-		int a,b;
+		int a,b,c;
 		SpringApplication.run(MiniProject01Application.class, args);
 	}
 
