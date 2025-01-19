8e3ee18e7fa2427811c37de86e58b0e2da235adc
mink <frog050406@gmail.com>
2025-01-19 18:18:21 +0900

    feat/2.spring기초. api

--- spring-project01/src/main/java/study/mini01/Controller/HelloController.java
+++ spring-project01/src/main/java/study/mini01/Controller/HelloController.java
@@ -2,8 +2,7 @@ package study.mini01.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
@@ -16,5 +15,31 @@ public class HelloController {
     public String helloMvc(@RequestParam("name")/*default:(value = "name", required = true)*/ String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody //http protocol의 body부
+    public String helloString(@RequestParam("name") String name){
+        return "hello"+name; // "hello spring"이 응답
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+   public Hello helloApi(@RequestParam("name") String name){
+
+    Hello hello = new Hello();
+    hello.setName(name);
+    return hello;
+    }
+    //객체 class Hello 생성
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
