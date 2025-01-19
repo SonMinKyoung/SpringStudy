2725df53d814e845d527df9897a66d37e6fef89c
mink <frog050406@gmail.com>
2025-01-19 17:46:54 +0900

    feat/02.spring 기초. static contents&mvc model

--- spring-project01/src/main/java/study/mini01/Controller/HelloController.java
+++ spring-project01/src/main/java/study/mini01/Controller/HelloController.java
@@ -3,6 +3,7 @@ package study.mini01.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,5 +11,10 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data","Nice to meet you");
         return "hello";
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name")/*default:(value = "name", required = true)*/ String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
