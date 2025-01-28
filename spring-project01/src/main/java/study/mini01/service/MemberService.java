192d9eea6350c9952fbbafe720bee857a9ba1032
mink <frog050406@gmail.com>
2025-01-28 20:02:52 +0900

    1(3)member search service

--- spring-project01/src/main/java/study/mini01/service/MemberService.java
+++ spring-project01/src/main/java/study/mini01/service/MemberService.java
@@ -4,6 +4,7 @@ import study.mini01.domain.Member;
 import study.mini01.repository.MemberRepository;
 import study.mini01.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -14,9 +15,8 @@ public class MemberService {
         /*Optional<Member> result= memberRepository.findbyName(member.getName());
         result.ifPresent(m -> {
             throw new IllegalStateException("이미 존재하는 회원입니다");
-        });*/
+        })*/
         validateDuplicateMember(member);
-
         memberRepository.save(member);
         return member.getId();
     }
@@ -26,5 +26,12 @@ public class MemberService {
                         .ifPresent(m -> {
                             throw new IllegalStateException("이미 존재하는 회원입니다");
                         });
+    }
+    /*전체회원 조회*/
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findbyId(memberId);
     }
 }
