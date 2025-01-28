fba2973861048b35d00ecde45b5d70607abbed05
mink <frog050406@gmail.com>
2025-01-28 19:57:21 +0900

    1(3)cmd+option+m ->method로 도출가능

--- spring-project01/src/main/java/study/mini01/service/MemberService.java
+++ spring-project01/src/main/java/study/mini01/service/MemberService.java
@@ -15,12 +15,16 @@ public class MemberService {
         result.ifPresent(m -> {
             throw new IllegalStateException("이미 존재하는 회원입니다");
         });*/
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
         memberRepository.findbyName(member.getName())
                         .ifPresent(m -> {
                             throw new IllegalStateException("이미 존재하는 회원입니다");
                         });
-
-        memberRepository.save(member);
-        return member.getId();
     }
 }
