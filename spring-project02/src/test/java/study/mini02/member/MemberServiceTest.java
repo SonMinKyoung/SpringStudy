133ff5301812671d4d62850b9e6214fdb168fc8f
mink <frog050406@gmail.com>
2025-02-05 21:14:11 +0900

    2(2)test code를 이용한 test

---
+++ spring-project02/src/test/java/study/mini02/member/MemberServiceTest.java
@@ -0,0 +1,19 @@
+package study.mini02.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    @Test //회원가입 test
+    void join(){
+        Member member = new Member(1L,"memberA", Grade.VIP);
+
+        memberService.join(member);
+        Member findMember = memberService.findMember(2L);
+
+        Assertions.assertThat(member).isEqualTo(findMember);
+
+    }
+}
